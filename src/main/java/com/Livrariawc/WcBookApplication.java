7ab0e92a3aeae41891015a548854d2443920512d
Willyan Fernando <Wff.cirino@gmail.com>
2021-07-22 00:43:55 -0300

    Primeiro commit

---
+++ src/main/java/com/Livrariawc/WcBookApplication.java
@@ -0,0 +1,13 @@
+package com.Livrariawc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Livrariawc/WcBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Livrariawc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
